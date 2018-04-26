0bb9710661ce7a51d703bcbf5e69806c03225c56
Jamie Shedley <jshedley@jscloud.co.uk>
2018-04-26 07:57:30 +0100

    test changes

--- JSCloud.LogPlayer.Tests/SqlHelperTests.cs
+++ JSCloud.LogPlayer.Tests/SqlHelperTests.cs
@@ -9,27 +9,26 @@ using System.Threading.Tasks;
 namespace JSCloud.LogPlayer.Tests
 {
     [TestFixture]
-    [Category("UnitTests")]
     public class SqlHelperTests
     {
 
-        [TestCase(typeof(int), SqlDbType.Int, Description = "Int >> Int")]
-        [TestCase(typeof(float), SqlDbType.Real, Description = "Float >> Real")]
-        [TestCase(typeof(double), SqlDbType.Float, Description = "Double >> Float")]
-        [TestCase(typeof(Guid), SqlDbType.UniqueIdentifier, Description = "Guid >> Uniqueidentifier")]
+        [TestCase(typeof(int), SqlDbType.Int, Description = "Int >> Int", Category = "UnitTests")]
+        [TestCase(typeof(float), SqlDbType.Real, Description = "Float >> Real", Category = "UnitTests")]
+        [TestCase(typeof(double), SqlDbType.Float, Description = "Double >> Float", Category = "UnitTests")]
+        [TestCase(typeof(Guid), SqlDbType.UniqueIdentifier, Description = "Guid >> Uniqueidentifier", Category = "UnitTests")]
         public void TestTypeOf(Type type, SqlDbType expectedType)
         {
             Assert.AreEqual(expectedType, SqlHelper.GetDbType(type));
         }
 
-        [TestCase(Description = "Generic Mapping")]
+        [TestCase(Description = "Generic Mapping", Category = "UnitTests")]
 
         public void TestGeneric()
         {
             Assert.AreEqual(SqlDbType.UniqueIdentifier, SqlHelper.GetDbType<Guid>());
         }
 
-        [TestCase(Description = "Invalid Mapping")]
+        [TestCase(Description = "Invalid Mapping", Category = "UnitTests")]
 
         public void TestInvalidMapping()
         {
